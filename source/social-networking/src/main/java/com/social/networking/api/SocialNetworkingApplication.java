f33600181b1674d797f4040025e742980419cca4
ToanNguyen <toannguyenit239@gmail.com>
2023-09-18 00:05:59 +0700

    Initialize Spring Boot project

---
+++ source/social-networking/src/main/java/com/social/networking/api/SocialNetworkingApplication.java
@@ -0,0 +1,12 @@
+package com.social.networking.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkingApplication.class, args);
+	}
+}
---
+++ source/social-networking/src/test/java/com/social/networking/api/socialnetworking/SocialNetworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.networking.api.socialnetworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
