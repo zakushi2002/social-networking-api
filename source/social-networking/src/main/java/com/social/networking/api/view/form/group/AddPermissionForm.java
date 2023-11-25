3bc79f2cbcd9cc3c4020a6798f8a789d16240961
ToanNguyen <toannguyenit239@gmail.com>
2023-11-25 12:36:39 +0700

    Back-end add-ons

---
+++ source/social-networking/src/main/java/com/social/networking/api/view/form/group/AddPermissionForm.java
@@ -0,0 +1,16 @@
+package com.social.networking.api.view.form.group;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class AddPermissionForm {
+    @ApiModelProperty(name = "id", required = true)
+    @NotNull(message = "id cannot be null!")
+    private Long id;
+    @ApiModelProperty(name = "permissions", required = true)
+    @NotNull(message = "permissions cannot be null!")
+    private Long[] permissions;
+}
