71becec77fdfa07617296430698546ee4eee9a59
ToanNguyen <toannguyenit239@gmail.com>
2024-03-20 01:25:20 +0700

    API CRUD for Community Member

---
+++ source/social-networking/src/main/java/com/social/networking/api/mapper/CommunityMemberMapper.java
@@ -0,0 +1,26 @@
+package com.social.networking.api.mapper;
+
+import com.social.networking.api.model.CommunityMember;
+import com.social.networking.api.view.dto.community.member.CommunityMemberDto;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        uses = {AccountMapper.class, CategoryMapper.class})
+public interface CommunityMemberMapper {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "account", target = "account", qualifiedByName = "fromAccountToAutoCompleteDto")
+    @Mapping(source = "community", target = "community", qualifiedByName = "fromEntityToShortDto")
+    @Mapping(source = "kind", target = "kind")
+    @Mapping(source = "createdDate", target = "createdDate")
+    @BeanMapping(ignoreByDefault = true)
+    @Named("fromEntityToDto")
+    CommunityMemberDto fromEntityToDto(CommunityMember communityMember);
+
+    @IterableMapping(elementTargetType = CommunityMemberDto.class, qualifiedByName = "fromEntityToDto")
+    @Named("fromEntityToDtoList")
+    List<CommunityMemberDto> fromEntityToDtoList(List<CommunityMember> communityMembers);
+}
--- source/social-networking/src/main/java/com/social/networking/api/repository/CommunityMemberRepository.java
+++ source/social-networking/src/main/java/com/social/networking/api/repository/CommunityMemberRepository.java
@@ -4,5 +4,8 @@ import com.social.networking.api.model.CommunityMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface CommunityMemberRepository extends JpaRepository<CommunityMember, Long>, JpaSpecificationExecutor<CommunityMember> {
+    Optional<CommunityMember> findByAccountIdAndCommunityId(Long accountId, Long communityId);
 }
