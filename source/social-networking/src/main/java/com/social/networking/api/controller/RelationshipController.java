68d4317bd51bc94ecf47fd342b0dd3dbdb2ba072
ToanNguyen <toannguyenit239@gmail.com>
2024-05-02 18:22:40 +0700

    Temp - fix permission api Relationship

--- source/social-networking/src/main/java/com/social/networking/api/controller/RelationshipController.java
+++ source/social-networking/src/main/java/com/social/networking/api/controller/RelationshipController.java
@@ -144,7 +144,7 @@ public class RelationshipController extends BaseController {
      * @return a list of relationships
      */
     @GetMapping(value = "/list-follower", produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("hasRole('FL_FROM_L')")
+    @PreAuthorize("hasRole('FL_FORM_L')")
     public ApiMessageDto<ResponseListDto<RelationshipDto>> listFollower(Pageable pageable) {
         ApiMessageDto<ResponseListDto<RelationshipDto>> apiMessageDto = new ApiMessageDto<>();
         Page<Relationship> followers = relationshipRepository.findAllByAccountId(getCurrentUser(), pageable);
@@ -172,7 +172,7 @@ public class RelationshipController extends BaseController {
     }
 
     @GetMapping(value = "/list-follower-by-account-id/{accountId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @PreAuthorize("hasRole('FL_FROM_L')")
+    @PreAuthorize("hasRole('FL_FORM_L')")
     public ApiMessageDto<ResponseListDto<RelationshipDto>> listFollower(@PathVariable("accountId") Long accountId, Pageable pageable) {
         ApiMessageDto<ResponseListDto<RelationshipDto>> apiMessageDto = new ApiMessageDto<>();
         Page<Relationship> followers = relationshipRepository.findAllByAccountId(accountId, pageable);
